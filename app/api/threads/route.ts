85c5236fd3396f2ad12830587813592d93440ba6
Conor <cyuen10@gmail.com>
2024-08-04 19:18:05 -0400

    Added Recaptcha logic back to thread creation endpoint

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -8,29 +8,29 @@ export async function POST(req: Request, res: NextApiResponse) {
   try {
     const formData = await req.formData();
 
-    // const token = formData.get('token') as string;
-    // const client = new RecaptchaEnterpriseServiceClient();
-    // const projectPath = client.projectPath('bran-1722634856780');
-    // const request = ({
-    //   assessment: {
-    //     event: {
-    //       token: token,
-    //       expectedAction: "USER_ACTION",
-    //       siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
-    //     }
-    //   },
-    //   parent: projectPath
-    // })
+    const token = formData.get('token') as string;
+    const client = new RecaptchaEnterpriseServiceClient();
+    const projectPath = client.projectPath('bran-1722634856780');
+    const request = ({
+      assessment: {
+        event: {
+          token: token,
+          expectedAction: "USER_ACTION",
+          siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
+        }
+      },
+      parent: projectPath
+    })
 
-    // const [ response ] = await client.createAssessment(request);
+    const [ response ] = await client.createAssessment(request);
 
-    // if (!response || !response.riskAnalysis || !response.riskAnalysis.score) {
-    //   return NextResponse.json({ message: "Failed to verify recaptcha" });
-    // }
+    if (!response || !response.riskAnalysis || !response.riskAnalysis.score) {
+      return NextResponse.json({ message: "Failed to verify recaptcha" });
+    }
 
-    // if (response.riskAnalysis.score < 0.5) {
-    //   return NextResponse.json({ message: "Failed to verify recaptcha" });
-    // }
+    if (response.riskAnalysis.score < 0.5) {
+      return NextResponse.json({ message: "Failed to verify recaptcha" });
+    }
 
     const image = formData.get('file') as unknown as File;
 
