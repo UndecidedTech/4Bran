3ddd3c342b1e6349bc66ab4875d4ffffbb0e8499
Conor <cyuen10@gmail.com>
2024-08-05 04:09:18 -0400

    Trying another auth method-3

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -4,62 +4,77 @@ import { prisma } from "@/utility/prisma";
 import { uploadImageToS3 } from "@/utility/functions";
 import { RecaptchaEnterpriseServiceClient } from "@google-cloud/recaptcha-enterprise";
 import fs from "fs";
+import axios from "axios";
 
 export async function POST(req: Request, res: NextApiResponse) {
   try {
     const formData = await req.formData();
 
     const token = formData.get('token') as string;
-    const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS || '', 'base64').toString('ascii'))
 
-    const client = new RecaptchaEnterpriseServiceClient({ credentials: credentials });
-    
-    const projectPath = client.projectPath('bran-1722634856780');
-    const request = ({
-      assessment: {
-        event: {
-          token: token,
-          expectedAction: "USER_ACTION",
-          siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
-        }
-      },
-      parent: projectPath
-    })
+    const url = `secret=${process.env.GOOGLE_RECAPTCHA_SECRET}&resposne=${token}`;
 
-    const [ response ] = await client.createAssessment(request);
-
-    if (!response || !response.riskAnalysis || !response.riskAnalysis.score) {
-      return NextResponse.json({ message: "Failed to verify recaptcha" });
-    }
-
-    if (response.riskAnalysis.score < 0.5) {
-      return NextResponse.json({ message: "Failed to verify recaptcha" });
-    }
-
-    const image = formData.get('file') as unknown as File;
-
-    if (!image) {
-      return NextResponse.json({ message: "No image uploaded" });
-    }
-
-    const imageData = await uploadImageToS3({ file: image, type: 'thread', fileName: formData.get('fileName') as string });
-
-    const thread = await prisma.post.create({
-      data: {
-        image: imageData.imageLink,
-        subject: formData.get('subject') as string,
-        comment: formData.get('comment') as string,
-        imageSize: imageData.fileSize,
-        imageResolution: imageData.resolution,
-        imageName: imageData.fileName,
-      },
-      select: {
-        id: true,
+    const res = await axios.post('https://www.google.com/recaptcha/api/siteverify', url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
       }
-    })
+    });
 
-    return NextResponse.json({ thread });
+    console.log(res.data);
 
+    if (res && res.data?.success && res.data?.score > 0.5) {
+      console.log('success');
+
+      // const client = new RecaptchaEnterpriseServiceClient({ credentials: credentials });
+      
+      // const projectPath = client.projectPath('bran-1722634856780');
+      // const request = ({
+      //   assessment: {
+      //     event: {
+      //       token: token,
+      //       expectedAction: "USER_ACTION",
+      //       siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
+      //     }
+      //   },
+      //   parent: projectPath
+      // })
+
+      // const [ response ] = await client.createAssessment(request);
+
+      // if (!response || !response.riskAnalysis || !response.riskAnalysis.score) {
+      //   return NextResponse.json({ message: "Failed to verify recaptcha" });
+      // }
+
+      // if (response.riskAnalysis.score < 0.5) {
+      //   return NextResponse.json({ message: "Failed to verify recaptcha" });
+      // }
+
+      const image = formData.get('file') as unknown as File;
+
+      if (!image) {
+        return NextResponse.json({ message: "No image uploaded" });
+      }
+
+      const imageData = await uploadImageToS3({ file: image, type: 'thread', fileName: formData.get('fileName') as string });
+
+      const thread = await prisma.post.create({
+        data: {
+          image: imageData.imageLink,
+          subject: formData.get('subject') as string,
+          comment: formData.get('comment') as string,
+          imageSize: imageData.fileSize,
+          imageResolution: imageData.resolution,
+          imageName: imageData.fileName,
+        },
+        select: {
+          id: true,
+        }
+      })
+
+      return NextResponse.json({ thread });
+    } else {
+      return NextResponse.json({ message: "Failed to verify recaptcha" });
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: "Error creating new post" });
